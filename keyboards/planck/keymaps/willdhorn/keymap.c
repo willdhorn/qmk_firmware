06c1ff9ac6a8e18c02c83a18b534a246cb07281e
willdhorn <none>
2022-02-14 16:48:27 -0800

    refactoring

--- keyboards/planck/keymaps/willdhorn/keymap.c
+++ keyboards/planck/keymaps/willdhorn/keymap.c
@@ -6,92 +6,42 @@
 #include "eeprom.h"
 #include "print.h"
 
+//#include "process_tap_hold.h"
+
 #include "key_defs.h"
 #include "layers.h"
-#include "tap_dances.h"
 #include "layouts.h"
 #include "rgb_stuff.h"
+#include "tap_dances.h"
 #include "user_debug.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENABLE_LAYOUT_QWERTY
-                                                              [_QWERTY]       = LAYER_QWERTY,
+        [_QWERTY]       = LAYER_QWERTY,
 #endif
 #ifdef ENABLE_LAYOUT_COLEMAK
-                                                              [_COLEMAK_DH]   = LAYER_COLEMAK_DH,
+        [_COLEMAK_DH]   = LAYER_COLEMAK_DH,
 #endif
 #ifdef ENABLE_LAYOUT_ISRT
-                                                              [_ISRT]         = LAYER_ISRT,
+        [_ISRT]         = LAYER_ISRT,
 #endif
 #ifdef ENABLE_LAYOUT_WORKMAN
-                                                              [_WORKMAN]      = LAYER_WORKMAN,
+        [_WORKMAN]      = LAYER_WORKMAN,
 #endif
-                                                              [_EXT]          = LAYER_EXT,
-                                                              [_SYM]          = LAYER_SYM,
-                                                              [_NUM]          = LAYER_NUM,
-                                                              [_VSCODE]       = LAYER_VSCODE,
-                                                              [_SWITCH]       = LAYER_SWITCH,
-                                                              [_WNDW_HALF]    = LAYER_WNDW_HALF,
-                                                              [_WNDW_QUAD]    = LAYER_WNDW_QUAD,
-                                                            //   [_WNDW_VERT]    = LAYER_WNDW_VERT,
-                                                              [_WNDW_THRD]    = LAYER_WNDW_THRD,
-                                                              [_WNDW_SIXT]    = LAYER_WNDW_SIXT,
-                                                              [_WNDW_NINT]    = LAYER_WNDW_NINT,
-                                                              [_ADJUST]       = LAYER_ADJUST,
-                                                              };
-
-extern bool         g_suspend_state;
-extern rgb_config_t rgb_matrix_config;
-extern led_config_t g_led_config;
-
-/*
-  === KEY OVERRIDES ===
-*/
-//const key_override_t delete_key_override = SHIFT_OVERRIDE(KC_BSPACE, KC_DELETE);
-
-const key_override_t period_question_override = SHIFT_OVERRIDE(KC_DOT, KC_QUES);
-const key_override_t period_question_alt_override = ALT_OVERRIDE(KC_DOT, KC_QUES);
-const key_override_t comma_exclamation_override = SHIFT_OVERRIDE(KC_COMMA, KC_EXLM);
-const key_override_t comma_exclamation_alt_override = ALT_OVERRIDE(KC_COMMA, KC_EXLM);
-const key_override_t quote_dquote_alt_override = ALT_OVERRIDE(KC_QUOT, KC_DQUO);
-
-// const key_override_t sym_lparen_lbracket_override = SHIFT_OVERRIDE(KC_LPRN, KC_LBRC);
-// const key_override_t sym_rparen_rbracket_override = SHIFT_OVERRIDE(KC_RPRN, KC_RBRC);
-const key_override_t sym_lbrace_langlebr_override = SHIFT_OVERRIDE(KC_LCBR, KC_LABK);
-const key_override_t sym_rbrace_ranglebr_override = SHIFT_OVERRIDE(KC_RCBR, KC_RABK);
-
-// const key_override_t sym_plus_carrot_override = SHIFT_OVERRIDE(KC_PLUS, KC_CRRT);
-// const key_override_t sym_astrick_dollar_override = SHIFT_OVERRIDE(KC_ASTR, KC_DLR);
-// const key_override_t sym_equal_tilde_override = SHIFT_OVERRIDE(KC_EQL, KC_TILD);
-// const key_override_t sym_minus_ampersand_override = SHIFT_OVERRIDE(KC_MINS, KC_AMPR);
-const key_override_t sym_slash_bsls_override = SHIFT_OVERRIDE(KC_SLSH, KC_BSLS);
-const key_override_t sym_colon_semicolon_override = SHIFT_OVERRIDE(KC_COLN, KC_SCLN);
-// const key_override_t sym_tilde_grave_override = SHIFT_OVERRIDE(KC_TILD, KC_GRV);
-
-const key_override_t sym_vscode_selection_override = SHIFT_OVERRIDE(VSC_SEL_EXPND, VSC_SEL_SHRNK);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    // &delete_key_override,
-    &period_question_override,
-    &comma_exclamation_override,
-    &period_question_alt_override,
-    &comma_exclamation_alt_override,
-    &quote_dquote_alt_override,
-    // &sym_lparen_lbracket_override,
-    // &sym_rparen_rbracket_override,
-    &sym_lbrace_langlebr_override,
-    &sym_rbrace_ranglebr_override,
-    // &sym_plus_carrot_override,
-    // &sym_astrick_dollar_override,
-    // &sym_equal_tilde_override,
-    // &sym_minus_ampersand_override,
-    // &sym_slash_bsls_override,
-    &sym_colon_semicolon_override,
-    // &sym_tilde_grave_override,
-    &sym_vscode_selection_override,
-    NULL // Null terminate the array of overrides!
+        [_EXT]          = LAYER_EXT,
+        [_SYM]          = LAYER_SYM,
+        [_NUM]          = LAYER_NUM,
+        [_VSCODE]       = LAYER_VSCODE,
+        [_SWITCH]       = LAYER_SWITCH,
+        [_WNDW_HALF]    = LAYER_WNDW_HALF,
+        [_WNDW_QUAD]    = LAYER_WNDW_QUAD,
+    //   [_WNDW_VERT]    = LAYER_WNDW_VERT,
+        [_WNDW_THRD]    = LAYER_WNDW_THRD,
+        [_WNDW_SIXT]    = LAYER_WNDW_SIXT,
+        [_WNDW_NINT]    = LAYER_WNDW_NINT,
+        [_ADJUST]       = LAYER_ADJUST,
 };
+
 
 
 // /*
@@ -138,6 +88,10 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 /*
   === START CONFIG ===
 */
+extern bool         g_suspend_state;
+extern rgb_config_t rgb_matrix_config;
+extern led_config_t g_led_config;
+
 void keyboard_post_init_user(void) {
     // debug_enable = true;
     //    debug_matrix = true;
@@ -148,30 +102,31 @@ void keyboard_post_init_user(void) {
     set_default_layer_colors(default_layer_state);
 }
 
-void rgb_matrix_indicators_user(void) {
-    set_all_layer_colors(layer_state);
-    // Set caps lock indicator light
-    if (host_keyboard_led_state().caps_lock) {
-        planck_ez_left_led_level(255);
-    } else {
-        planck_ez_left_led_level(0);
-    }
-}
-
 static uint16_t bspace_timer = 0;
 extern uint16_t mod_tap_timer;
 extern uint8_t mod_tap_active;
+
+extern bool shift_down;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bool pressed = record->event.pressed;
     uint8_t mod_state = get_mods();
     uint8_t osm_mod_state = get_oneshot_mods();
+
     switch (keycode) {
         // Keycodes defined in custom_keycodes
         case KC_EMPTY:  // used for layer coloring; fundtionally identical to KC_NO
             return false;
+        case OSM(MOD_LSFT):
+        case KC_LSFT:
+            if (pressed) {
+                shift_down = true;
+            } else {
+                shift_down = false;
+            }
+            return true;
         case KC_BACKSPACE:
-            // because key overrides cant handle this
+            // because key overrides cant haendle this
             if (pressed) {
                 bspace_timer = record->event.time;
                 return false;
@@ -322,19 +277,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     state = update_tri_layer_state(state, _SWITCH, _EXT, _VSCODE);
-//     state = update_tri_layer_state(state, _NUM, _SWITCH, _ADJUST);
-//     return state;
-// }
-
-void vscode_chord(uint16_t kc) {
-    two_tap(CMD(KC_K), kc);
-}
-
-void two_tap(uint16_t kc1, uint16_t kc2) {
-    tap_code16(kc1);
-    tap_code16(kc2);
+void matrix_scan_user(void) {
+#ifdef AUDIO_ENABLE
+    matrix_scan_audio_user();
+#endif
 }
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -354,10 +300,39 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
       case OSX_APP_WNDW:
       case OSX_APP_NEXT:
           return 140;
+      case T_BSPACE:
+          return 175;
       default:
           return TAPPING_TERM;
   }
 }
+
+void rgb_matrix_indicators_user(void) {
+    set_all_layer_colors(layer_state);
+    // Set caps lock indicator light
+    if (host_keyboard_led_state().caps_lock) {
+        planck_ez_left_led_level(255);
+    } else {
+        planck_ez_left_led_level(0);
+    }
+}
+
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     state = update_tri_layer_state(state, _SWITCH, _EXT, _VSCODE);
+//     state = update_tri_layer_state(state, _NUM, _SWITCH, _ADJUST);
+//     return state;
+// }
+
+void vscode_chord(uint16_t kc) {
+    two_tap(CMD(KC_K), kc);
+}
+
+void two_tap(uint16_t kc1, uint16_t kc2) {
+    tap_code16(kc1);
+    tap_code16(kc2);
+}
+
+
 
 // bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
 //     switch (keycode) {
@@ -375,75 +350,3 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 //         return false;
 //     }
 // }
-
-// Audio and Music
-
-#ifdef AUDIO_ENABLE
-bool     muse_mode      = false;
-uint8_t  last_muse_note = 0;
-uint16_t muse_counter   = 0;
-uint8_t  muse_offset    = 70;
-uint16_t muse_tempo     = 50;
-
-void encoder_update(bool clockwise) {
-    if (muse_mode) {
-        if (IS_LAYER_ON(_RAISE)) {
-            if (clockwise) {
-                muse_offset++;
-            } else {
-                muse_offset--;
-            }
-        } else {
-            if (clockwise) {
-                muse_tempo += 1;
-            } else {
-                muse_tempo -= 1;
-            }
-        }
-    } else {
-        if (clockwise) {
-#    ifdef MOUSEKEY_ENABLE
-            register_code(KC_MS_WH_DOWN);
-            unregister_code(KC_MS_WH_DOWN);
-#    else
-            register_code(KC_PGDN);
-            unregister_code(KC_PGDN);
-#    endif
-        } else {
-#    ifdef MOUSEKEY_ENABLE
-            register_code(KC_MS_WH_UP);
-            unregister_code(KC_MS_WH_UP);
-#    else
-            register_code(KC_PGUP);
-            unregister_code(KC_PGUP);
-#    endif
-        }
-    }
-}
-
-bool music_mask_user(uint16_t keycode) {
-    switch (keycode) {
-        case RAISE:
-        case LOWER:
-            return false;
-        default:
-            return true;
-    }
-}
-#endif
-
-void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
-        }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    }
-#endif
-}
--- keyboards/planck/keymaps/willdhorn/rules.mk
+++ keyboards/planck/keymaps/willdhorn/rules.mk
@@ -3,6 +3,7 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 SRC += tap_dances.c
 SRC += rgb_stuff.c
+SRC += key_overrides.c
 CONSOLE_ENABLE = yes
 COMMAND_ENABLE = no
 AUDIO_ENABLE = no
